e3c545daeb4fcaa2fd4cef65045e7c7d7e19e81f
brad <brad_124@hotmail.co.uk>
2017-06-11 16:42:16 +0100

    created skeleton

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const pug = require('pug');
+const path = require('path');
+
+const app = express();
+
+
+// load our view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// home route
+app.get('/', function(req, res){
+    res.render('index', {
+      title: 'hello there'
+    })
+})
+
+
+
+// start our server
+app.listen(3000, function() {
+  console.log('started server on port 3000');
+});
